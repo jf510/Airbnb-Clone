b8897a3291313a8b78d2c56636ce7d56d494de5c
Jfontaine <jfontaine19@gmail.com>
2022-05-20 18:51:15 -0400

    created state for date search. added onclick function to banner search

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Banner.css";
+import Search from './Search'
+import Button from "@mui/material/Button";
 
 function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div className="banner">
-      <h1>Banner Here!</h1>
+      <div className="banner__search">
+        {showSearch && <Search />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          className="search__button"
+          varient="outlined"
+        >
+          Search Dates
+        </Button>
+      </div>
+      <div className="banner__info">
+        <h1>Get Out and Stretch Your Imagination</h1>
+        <h5>
+          Plan a different kind of getaway to uncover the hidden gems near you!
+        </h5>
+        <Button varient="outlined">Explore Nearby</Button>
+      </div>
     </div>
   );
 }
