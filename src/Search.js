fb2e34674e2a263c070928302dc0ddfa676da8cc
Jfontaine <jfontaine19@gmail.com>
2022-05-20 18:48:56 -0400

    created search component. Imported daterangepicker from material ui. created state for the date picker

---
+++ src/Search.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { Calendar } from "react-date-range";
+import "./Search.css";
+
+function Search() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  return <div className="search"></div>;
+}
+
+export default Search;
